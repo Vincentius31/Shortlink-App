5371f1b0efabc117ae461ddc85dc4435b59b80cb
Vincentius <vincentius85@gmail.com>
2026-04-07 17:49:28 +0700

    feat: adding endpoint for create short link

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"shortlink-app/internal/di"
+	"shortlink-app/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -11,10 +12,17 @@ func SetupRoutes(r *gin.Engine, conn *pgxpool.Pool) {
 	container := di.NewContainer(conn)
 
 	authHandler := container.AuthHandler()
+	linkHandler := container.LinkHandler()
 
-	authRoutes := r.Group("/auth")
+	authRoutes := r.Group("/api")
 	{
 		authRoutes.POST("/register", authHandler.Register)
 		authRoutes.POST("/login", authHandler.Login)
+	}
+
+	linkRoutes := r.Group("/api")
+	linkRoutes.Use(middleware.AuthMiddleware())
+	{
+		linkRoutes.POST("/links", linkHandler.Create)
 	}
 }
