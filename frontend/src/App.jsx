e56ba1aeaf67e1f998b219329464bec06f6232b8
Vincentius <vincentius85@gmail.com>
2026-04-08 00:13:28 +0700

    feat: adding routes using react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+import CreateLinkPage from './pages/CreateLinkPage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
-export default function App() {
+function App() {
   return (
-    <div className='text-5xl'> App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        
+        {/* Protected Routes */}
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/create-link" element={
+          <ProtectedRoute>
+            <CreateLinkPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        } />
+        
+        {/* 404 Page */}
+        <Route path="*" element={<NotFoundPage/>} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
