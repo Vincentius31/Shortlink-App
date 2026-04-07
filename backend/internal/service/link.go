a111f44fb28610dd4a8043c85064bd0fb81cd631
Vincentius <vincentius85@gmail.com>
2026-04-07 17:33:32 +0700

    feat: adding generate random slug and create short link service

---
+++ backend/internal/service/link.go
@@ -0,0 +1,89 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+	"shortlink-app/internal/model"
+	"shortlink-app/internal/repository"
+	"time"
+)
+
+type LinkService struct {
+	repo *repository.LinkRepository
+}
+
+func NewLinkService(repo *repository.LinkRepository) *LinkService {
+	return &LinkService{
+		repo: repo,
+	}
+}
+
+//getBaseURL returns the base URL for short links
+func getBaseURL() string {
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+
+	return fmt.Sprintf("http://%s:%s", host, port)
+}
+
+// Generated Random Slug: Created a random alphanumeric slug
+func generatedRandomSlug(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[r.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+// Create a new shortlink
+func (s *LinkService) Create(ctx context.Context, userID int, req model.CreateLinkRequest) (model.LinkResponse, error) {
+	var slug string
+
+	if req.Slug != "" {
+		exists, err := s.repo.SlugExists(ctx, slug)
+		if err != nil {
+			return model.LinkResponse{}, err
+		}
+		if exists {
+			return model.LinkResponse{}, errors.New("Slug already taken")
+		}
+		slug = req.Slug
+	}else {
+		for i := 0; i < 10; i++ {
+			slug = generatedRandomSlug(6)
+			exists, err := s.repo.SlugExists(ctx, slug)
+			if err != nil {
+				return model.LinkResponse{}, err
+			}
+			if !exists {
+				break
+			}
+		}
+	}
+
+	link := model.Link{
+		UserID: userID,
+		OriginalURL: req.OriginalURL,
+		Slug: slug,
+		CreatedAt: time.Now(),
+	}
+
+	if err := s.repo.Create(ctx,link); err != nil {
+		return model.LinkResponse{}, err
+	}
+
+	baseURL := getBaseURL()
+
+	return model.LinkResponse{
+		OriginalURL: link.OriginalURL,
+		Slug: link.Slug,
+		ShortURL: baseURL + "/" + link.Slug,
+		CreatedAt: link.CreatedAt.Format(time.RFC3339),
+	}, nil
+}
