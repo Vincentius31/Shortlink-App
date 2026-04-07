bfb6582988231795c81d20ba360ff8f71e8544cb
Vincentius <vincentius85@gmail.com>
2026-04-07 16:35:43 +0700

    fix: fixing the structure of the response and adjust it for the login response

--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -10,8 +10,8 @@ type User struct {
 }
 
 type RegisterRequest struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Email           string `json:"email" binding:"required"`
+	Password        string `json:"password" binding:"required"`
 	ConfirmPassword string `json:"confirm_password" binding:"required"`
 }
 
@@ -20,8 +20,12 @@ type LoginRequest struct {
 	Password string `json:"password" binding:"required"`
 }
 
+type UserBrief struct {
+	ID int    `json:"user_id"`
+	Email  string `json:"email"`
+}
+
 type LoginResponse struct {
-	Token   string `json:"token"`
-	UserID  int    `json:"user_id"`
-	Email   string `json:"email"`
+	Token string    `json:"token"`
+	User  UserBrief `json:"user"`
 }
