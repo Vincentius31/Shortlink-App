0ed0cada8656149b4840d4eba40b3991dab204f4
Vincentius <vincentius85@gmail.com>
2026-04-07 17:48:24 +0700

    feat: combine repo, service and handler for link endpoint

--- backend/internal/di/container.go
+++ backend/internal/di/container.go
@@ -15,6 +15,11 @@ type Container struct {
 	userRepo    *repository.UserRepository
 	userService *service.UserService
 	authHandler *handlers.AuthHandler
+
+	//Links
+	linkRepo    *repository.LinkRepository
+	linkService *service.LinkService
+	linkHandler *handlers.LinkHandler
 }
 
 func NewContainer(pool *pgxpool.Pool) *Container {
@@ -32,8 +37,17 @@ func (c *Container) initDependencies() {
 	c.userRepo = repository.NewUserRepository(c.pool)
 	c.userService = service.NewUserService(c.userRepo)
 	c.authHandler = handlers.NewAuthHandler(c.userService)
+
+	//Links
+	c.linkRepo = repository.NewLinkRepository(c.pool)
+	c.linkService = service.NewLinkService(c.linkRepo)
+	c.linkHandler = handlers.NewLinkHandler(c.linkService)
 }
 
 func (c *Container) AuthHandler() *handlers.AuthHandler {
 	return c.authHandler
+}
+
+func (c *Container) LinkHandler() *handlers.LinkHandler {
+	return c.linkHandler
 }
