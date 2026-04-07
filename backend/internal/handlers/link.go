aa1a2435b342e030ba27a444d12baaf05848d782
Vincentius <vincentius85@gmail.com>
2026-04-07 17:45:44 +0700

    feat: adding handler for the create links

---
+++ backend/internal/handlers/link.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"net/http"
+	"shortlink-app/internal/model"
+	"shortlink-app/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LinkHandler struct {
+	linkService *service.LinkService
+}
+
+func NewLinkHandler(ls *service.LinkService) *LinkHandler {
+	return &LinkHandler{
+		linkService: ls,
+	}
+}
+
+func (h *LinkHandler) Create(ctx *gin.Context) {
+	var req model.CreateLinkRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, model.WebResponse{
+			Success: false,
+			Message: err.Error(),
+			Results: nil,
+		})
+		return
+	}
+
+	userID, exists := ctx.Get("user_id")
+	if !exists {
+		ctx.JSON(http.StatusUnauthorized, model.WebResponse{
+			Success: false,
+			Message: "Unauthorized, Please Login first!",
+			Results: nil,
+		})
+		return
+	}
+
+	response, err := h.linkService.Create(ctx.Request.Context(), userID.(int), req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, model.WebResponse{
+			Success: false,
+			Message: err.Error(),
+			Results: nil,
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, model.WebResponse{
+		Success: true,
+		Message: "Link created successfully",
+		Results: response,
+	})
+}
