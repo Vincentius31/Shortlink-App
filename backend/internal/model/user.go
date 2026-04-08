12fb5bb1da57f7b921d589a1b7cbe3260b02d8c1
Vincentius <vincentius85@gmail.com>
2026-04-08 14:39:03 +0700

    fix: fixing the models of struct User and adding new Struct for response of update User Profile

--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -3,10 +3,22 @@ package model
 import "time"
 
 type User struct {
-	ID           int       `json:"id" db:"id"`
-	Email        string    `json:"email" db:"email"`
-	PasswordHash string    `json:"-" db:"password_hash"`
-	CreatedAt    time.Time `json:"created_at" db:"created_at"`
+	ID             int       `json:"id" db:"id"`
+	Email          string    `json:"email" db:"email"`
+	PasswordHash   string    `json:"-" db:"password_hash"`
+	FullName       *string   `json:"full_name" db:"full_name"`
+	Bio            *string   `json:"bio" db:"bio"`
+	ProfilePicture *string   `json:"profile_picture" db:"profile_picture"`
+	CreatedAt      time.Time `json:"created_at" db:"created_at"`
+}
+
+type UserProfileResponse struct {
+	Email          string `json:"email"`
+	FullName       string `json:"full_name"`
+	Bio            string `json:"bio"`
+	ProfilePicture string `json:"profile_picture"`
+	LinkCount      int    `json:"link_count"`
+	CreatedAt      string `json:"created_at"`
 }
 
 type RegisterRequest struct {
@@ -21,8 +33,8 @@ type LoginRequest struct {
 }
 
 type UserBrief struct {
-	ID int    `json:"user_id"`
-	Email  string `json:"email"`
+	ID    int    `json:"user_id"`
+	Email string `json:"email"`
 }
 
 type LoginResponse struct {
